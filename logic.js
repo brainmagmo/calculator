30a29ab3f667bf3e052921cac4d97cee4e7a3423
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2021-12-08 11:07:56 -0800

    added error message to calculator, cleaned up comments

--- logic.js
+++ logic.js
@@ -1,18 +1,19 @@
 var keys =  [...document.getElementsByTagName('form')[0].getElementsByTagName('input')];//make [0] more defined
-//console.log(keys);
+
 var nums = keys.filter(e => e.hasAttribute('class') && e.getAttribute('class')=='num');
-//console.log(nums.map(e=>e.getAttribute('value')));
+
 var ops = keys.filter(e => e.hasAttribute('class') && e.getAttribute('class')=='operator');
-//console.log(ops.map(e=>e.getAttribute('value')));
+
 var return_key = keys.filter(e => e.name == 'doit')[0];//should be unique
-//console.log(return_key.value);
+
 var display = keys.filter(e => e.id == 'display')[0];//must be unique
-//console.log(display.value);
+
 var clear_button = keys.filter(e => e.id == 'clear')[0];
-//console.log(clear_button.value);
+
 nums.forEach(num => num.addEventListener("click", function() {
     display.value += num.value;
 }));
+
 ops.forEach(op => op.addEventListener("click", function() {
     switch (op.name) {
         case('times'):
@@ -25,9 +26,18 @@ ops.forEach(op => op.addEventListener("click", function() {
             display.value += op.value;
     }
 }));
+
 return_key.addEventListener("click", function() {
-    display.value = eval(display.value);
+    let result;
+    try {
+        result = eval(display.value);
+    } catch {
+        result = 'ERROR'
+    } finally {
+        display.value = result;
+    }
 })
+
 clear_button.addEventListener("click", function() {
     display.value = '';
 })

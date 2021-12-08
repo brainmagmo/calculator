2c2324413e8b3e37306be3f90b81983406af074b
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2021-12-08 11:42:57 -0800

    condensed code, added parens and dot

--- logic.js
+++ logic.js
@@ -1,43 +1,52 @@
-var keys =  [...document.getElementsByTagName('form')[0].getElementsByTagName('input')];//make [0] more defined
+var keys = [...document.getElementsByTagName('form')[0].getElementsByTagName('input')];//make [0] more defined
 
-var nums = keys.filter(e => e.hasAttribute('class') && e.getAttribute('class')=='num');
-
-var ops = keys.filter(e => e.hasAttribute('class') && e.getAttribute('class')=='operator');
-
-var return_key = keys.filter(e => e.name == 'doit')[0];//should be unique
-
-var display = keys.filter(e => e.id == 'display')[0];//must be unique
-
-var clear_button = keys.filter(e => e.id == 'clear')[0];
-
-nums.forEach(num => num.addEventListener("click", function() {
-    display.value += num.value;
-}));
-
-ops.forEach(op => op.addEventListener("click", function() {
-    switch (op.name) {
-        case('times'):
-            display.value += '*';
+function addListenerToKeys(key) {
+    switch (key.name) {
+        case ('times'):
+            key.addEventListener("click", function () {
+                display.value += '*';
+            })
             break;
-        case('divide'):
-            display.value += '/';
+        case ('divide'):
+            key.addEventListener("click", function () {
+                display.value += '/';
+            })
+            break;
+        case ('plus'):
+            key.addEventListener("click", function () {
+                display.value += '+';
+            })
+            break;
+        case ('minus'):
+            key.addEventListener("click", function () {
+                display.value += '-';
+            })
+            break;
+        case ('clear'):
+            key.addEventListener("click", function () {
+                display.value = '';
+            })
+            break;
+        case ('return'):
+            key.addEventListener("click", function () {
+                let result;
+                try {
+                    result = eval(display.value);
+                } catch {
+                    result = 'ERROR'
+                } finally {
+                    if (isNaN(result)) {
+                        result = 'ERROR'
+                    };
+                    display.value = result;
+                }
+            });
             break;
         default:
-            display.value += op.value;
+            key.addEventListener("click", function () {
+                display.value += key.value;
+            });
     }
-}));
+}
 
-return_key.addEventListener("click", function() {
-    let result;
-    try {
-        result = eval(display.value);
-    } catch {
-        result = 'ERROR'
-    } finally {
-        display.value = result;
-    }
-})
-
-clear_button.addEventListener("click", function() {
-    display.value = '';
-})
+keys.forEach(addListenerToKeys);
